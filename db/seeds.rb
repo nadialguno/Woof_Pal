ae6b87491ad253cfeb59520f3cd6fb4dcfcaa5cd
jose garcia <joey.garcia3711@gmail.com>
2022-05-30 17:24:38 -0500

    Seed and schedule path

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(
+  email: "user@user.com",
+  password: "123456"
+)
+
+puppy = Dog.create!(
+  born_on: Date.today - 14.days,
+  user: user,
+  name: "Youngin"
+)
+
+dog = Dog.create!(
+  born_on: Date.today - 8.months,
+  user: user,
+  name: "Adolin"
+)
+
+old_mutt = Dog.create!(
+  born_on: Date.today - 8.months,
+  user: user,
+  name: "Old Mutt"
+)
