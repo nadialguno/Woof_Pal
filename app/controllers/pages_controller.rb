4c98bf999e94eb84087b9b26f579c8836fc624da
Nadia Isela Angulo Martinez de la Vega <nayita7_5@hotmail.com>
2022-06-01 16:55:19 -0500

    Select color pallette

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :kitchen_sink ]
 
   def home
   end
