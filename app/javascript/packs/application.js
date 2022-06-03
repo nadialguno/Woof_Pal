ceeeca648cfe7f83ee7c189cf34eeba009fc15ea
jose garcia <joey.garcia3711@gmail.com>
2022-06-03 12:09:33 -0500

    Merge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@lottiefiles/lottie-player";
 
 Rails.start()
 Turbolinks.start()
