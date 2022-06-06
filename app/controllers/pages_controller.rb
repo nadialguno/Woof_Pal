76e97f9c433d3057a5ffc5e1d12ee65c82adfbb6
jose garcia <joey.garcia3711@gmail.com>
2022-06-06 17:13:48 -0500

    90% done with homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :kitchen_sink ]
 
   def home
+    @team_members = [
+      {
+        name: "Jose Garcia",
+        image: "https://avatars.githubusercontent.com/u/102878262?v=4",
+      },
+      {
+        name: "Nadia Isela",
+        image: "https://ca.slack-edge.com/T02NE0241-U03AYCT88MT-f39428141082-512",
+      },
+      {
+        name: "Joel Pickell",
+        image: "https://ca.slack-edge.com/T02NE0241-U03BDRMC0GZ-78c09a789fc8-512",
+      }
+    ]
   end
 end
