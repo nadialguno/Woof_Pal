4ad1bd09450ef02f37d9a0b1053c0cd36fd9ec6b
joel pickell <konstrictor@gmail.com>
2022-06-06 18:23:36 -0500

    Showed more than next three appt

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,13 +1,18 @@
 class Schedule
   attr_reader :dog
 
-  def initialize(dog, current_day = Date.today)
+  def initialize(dog, kept_count = 2, current_day = Date.today)
     @dog = dog
+    @kept_count = kept_count
     @current_day = current_day
   end
 
   def next_appointments
-    @dog.appointments.after(@current_day).order(scheduled_on: :asc)
+    @dog
+      .appointments
+      .after(@current_day)
+      .order(scheduled_on: :asc)
+      .distinct(:type)
     # + next 3
   end
 
@@ -16,5 +21,13 @@ class Schedule
     Appointment::Grooming.next(@dog).save! unless next_appointments.grooming.exists?
     Appointment::Vaccination.next(@dog).save! unless next_appointments.vaccination.exists?
     @dog.reload
+
+    [
+      next_appointments.deworming.first,
+      next_appointments.grooming.first,
+      next_appointments.vaccination.first
+    ].each do |appointment|
+      @dog.appointments << appointment.next unless appointment.has_next?(@kept_count - 1)
+    end
   end
 end
