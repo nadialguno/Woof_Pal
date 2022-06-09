8ad7132a3047fa7e7ff51151e9da6e24c961b457
joel pickell <konstrictor@gmail.com>
2022-06-09 12:14:40 -0500

    Patch flatpickr out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@lottiefiles/lottie-player";
-import { initFlatpickr } from "../plugins/flatpickr";
-
-initFlatpickr();
 
 Rails.start()
 Turbolinks.start()
